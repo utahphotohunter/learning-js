8b2f8031b9e33d773816b7ebf65d4217090ef557
utahphotohunter <mitch.hunter1197@gmail.com>
2024-06-28 13:40:46 -0600

    operators

---
+++ variables-data-types/operators/script.js
@@ -0,0 +1,26 @@
+// Operators
+
+// --------------------------------------------------------------------------
+
+// arithmetic operators
+
+let x;
+
+x = 5 + 5;
+console.log(x);
+
+x = x - 5;
+console.log(x);
+
+x = x * 5;
+console.log(x);
+
+x = x / 5;
+console.log(x);
+
+// value % value will return the remainder of the quotient
+x = x % 5;
+console.log(x);
+
+x = 7 % 5;
+console.log(x);
